ffde1b2b3c5ac1371bb02063e22d93405efebb52
Madush99 <madusankagajadeera99@gmail.com>
2021-10-12 14:28:51 +0530

    formatted with prettier

--- src/reducers/movieReducers.js
+++ src/reducers/movieReducers.js
@@ -8,12 +8,10 @@ export const movieListreducer = (state = {movies: []}, action) => {
   switch (action.type) {
     case MOVIE_LIST_REQUEST:
       return {loading: true};
-
     case MOVIE_LIST_SUCCESS:
       return {loading: false, movies: action.payload};
     case MOVIE_LIST_FAIL:
       return {movies: []};
-
     default:
       return state;
   }
