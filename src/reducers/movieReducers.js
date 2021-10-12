a7b3b711ba7a5c6aaff10a0870e0a1e2e771609a
Madush99 <madusankagajadeera99@gmail.com>
2021-10-12 14:27:53 +0530

    movie reducers

---
+++ src/reducers/movieReducers.js
@@ -0,0 +1,20 @@
+import {
+  MOVIE_LIST_FAIL,
+  MOVIE_LIST_REQUEST,
+  MOVIE_LIST_SUCCESS,
+} from '../constants/movieConstants';
+
+export const movieListreducer = (state = {movies: []}, action) => {
+  switch (action.type) {
+    case MOVIE_LIST_REQUEST:
+      return {loading: true};
+
+    case MOVIE_LIST_SUCCESS:
+      return {loading: false, movies: action.payload};
+    case MOVIE_LIST_FAIL:
+      return {movies: []};
+
+    default:
+      return state;
+  }
+};
