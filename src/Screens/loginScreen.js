605177ec6e764fc069d6f50b46986201a4a978d4
Madush99 <madusankagajadeera99@gmail.com>
2021-10-13 10:38:42 +0530

    user store

---
+++ src/Screens/loginScreen.js
@@ -0,0 +1,10 @@
+import React, {useState, useEffect} from 'react';
+
+const LoginScreen = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  return <div></div>;
+};
+
+export default LoginScreen;
--- src/store.js
+++ src/store.js
@@ -2,9 +2,11 @@ import {createStore, combineReducers, applyMiddleware} from 'redux';
 import thunk from 'redux-thunk';
 import {composeWithDevTools} from 'redux-devtools-extension';
 import {movieListreducer} from './reducers/movieReducers';
+import {userLoginReducer} from './reducers/userReducers';
 
 const reducer = combineReducers({
   movieList: movieListreducer,
+  userLogin: userLoginReducer,
 });
 
 const middleware = [thunk];
