5ddd8b970c758d5cdf0be57f4b78496663018bf0
Madush99 <madusankagajadeera99@gmail.com>
2021-10-14 10:44:08 +0530

    logout reducers

--- src/reducers/userReducers.js
+++ src/reducers/userReducers.js
@@ -1,5 +1,6 @@
 import {
   USER_LOGIN_FAIL,
+  USER_LOGIN_LOGOUT,
   USER_LOGIN_REQUEST,
   USER_LOGIN_SUCCESS,
 } from '../constants/userConstants';
@@ -12,6 +13,8 @@ export const userLoginReducer = (state = {}, action) => {
       return {loading: false, userInfo: action.payload};
     case USER_LOGIN_FAIL:
       return {loading: false, error: action.payload};
+    case USER_LOGIN_LOGOUT:
+      return {};
     default:
       return state;
   }
